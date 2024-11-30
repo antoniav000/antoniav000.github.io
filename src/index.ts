79330d2532d0b1d2d5edbfec1d10eb6a28376b02
Antonia Vazquez <antoniav@udel.edu>
2024-11-30 14:12:32 -0500

    the grinch content added

--- src/index.ts
+++ src/index.ts
@@ -119,11 +119,20 @@ interface DayContent {
     links: [{ text: "What's This? (from The Nightmare Before Christmas", url: "https://youtu.be/o36k8upu3Ks?feature=shared" },
       { text: "Krampus", url: "https://youtu.be/VbkGuCozc9M?feature=shared" },] },
   
+  { image: "assets/seusstree.jpg", 
+    message: `The Grinch is a cartoon character from authors Dr. Suess’s “The Grinch” book, published in 1957. 
+    In his story, the Grinch is a mean hateful creature who despises Christmas. The village below where he lives 
+    is called Whoville, and it has many creatures who love and always celebrate Christmas. Since he hates the Whos, 
+    on Christmas Eve he decides to go from house to house to steal all of their gifts in decorations, thinking “haha! 
+    Imagine how rotten they will feel in the morning!” To his surprise, when they wake up, they still see Christmas 
+    carols and enjoy the holiday, and he realizes that the holiday sentiment is more than just the food and presents, 
+    and his heart grows three sizes. Because of this story, people sometimes still refer to a hateful person as a “grinch.”`,
+    links: [{ text: "How the Grinch Stole Christmas", url: "https://dai.ly/x3ie7xt" }, 
+      {text: "Grinch Wikipedia", url: "https://en.wikipedia.org/wiki/Grinch"}] },
+
+  { image: "assets/presentsleigh.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
   { image: "assets/store.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
-  { image: "assets/nutcracker.jpeg", message: "The Nutcracker is a holiday story and ballet, celebrated as a Christmas tradition. It follows the magical journey of a young girl, Clara, who receives a nutcracker doll on Christmas Eve and finds herself in a world of dancing sweets and a fierce battle with the Mouse King. Set to Tchaikovsky’s music, The Nutcracker brings to life characters like the Sugar Plum Fairy and the Nutcracker Prince, creating an experience filled with wonder and fantasy.",
-      links: [{ text: "A Brief History of The Nutcracker", url: "https://youtu.be/jcbEMpnr-og?feature=shared" }, {text: "Dance of the Sugar Plum Fairy", url: "https://youtu.be/zV1qLYukTH8?feature=shared"}] },
-  { image: "assets/presentsleigh.jpeg", message: "here is the message", links: [{ text: "Sleigh Ride", url: "https://open.spotify.com/track/7fUYUcWy853HS8YJn0b3Sr?si=c322984b259b4225" },
-      {text:"A Christmas Story - The Leg Lamp is Delivered", url: "https://youtu.be/F-Vz67p5kLQ?feature=shared"}] },
+  { image: "assets/presentsleigh.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
   { image: "assets/presentsleigh.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
   { image: "assets/youllshootyoureyeout.jpg", message: "message", links: [{ text: "A Christmas Story - The Leg Lamp", url: "https://youtu.be/F-Vz67p5kLQ?feature=shared" }] },
   { image: "assets/presentsleigh.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
