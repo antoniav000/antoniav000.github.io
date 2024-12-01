1d5b1a4c6d6a88c8f7c36a74bbf34db0df2a1449
Antonia Vazquez <antoniav@udel.edu>
2024-11-30 20:32:57 -0500

    final index.ts content and html changes

--- src/index.ts
+++ src/index.ts
@@ -21,28 +21,28 @@ interface DayContent {
     Eve, giving presents to children around the globe. Legend has it that he keeps two lists;
     the “naughty list” and the “nice list.” Those whose names are on the nice list receive gifts
     for Christmas, but those who are on the naughty list only get a lump of coal. There are various
-    things you can do to land yourself on the naughty list, including (but not limited to) stealing
-    drinks from school restaurants, exercising morning yoga, and not reading books that your 
-    girlfriend gifted you for Christmas.`,
+    things you can do to land yourself on the naughty list, including sneaking drinks from school restaurants, 
+    exercising morning yoga, and not reading books that your girlfriend gifted you for Christmas.`,
     links: [{ text: "How Coca-Cola Took Over Santa Claus", url: "https://youtu.be/xEbVqBVC4u8?feature=shared" },
       { text: "Twas the Night Before Christmas Poem", url: "https://www.poetryfoundation.org/poems/43171/a-visit-from-st-nicholas" },
-      { text: "Santa Claus is Coming to Town", url: "https://open.spotify.com/track/6syO6UTl8QiiWADJy3Uy2d?si=a87fa2fd6be148ae" }] },
+      { text: "Santa Claus is Coming to Town by Frank Sinatra", url: "https://open.spotify.com/track/6syO6UTl8QiiWADJy3Uy2d?si=a87fa2fd6be148ae" },
+      { text: "Must Be Santa by Bob Dylan", url: "https://youtu.be/a8qE6WQmNus?feature=shared"}] },
 
   { image: "assets/snowman.jpg", message: `Snowmen are a big part of winter, and so of course there are
     a lot of Christmas songs and stories about them. “Frosty the Snowman” is a story about a snowman who 
     comes to life after being given a magician's hat to wear on his head haha. The second link, “The Snowman,” 
-    is a famous illustrated story about a snowman that comes to life and spends the night with his creator, a boy. 
-    The flight scene (at 15:10) is the most well-known part, and a reference to it will show up later in the calendar. 
-    The final link is a Christmas song by Sia that was popular when it came out because there was a trend where couples
-    would dance to it haha.`, 
+    is a famous illustrated story about a snowman that comes to life and spends the night with his creator. 
+    You don't have to watch the whole thing, but flight scene (at 15:10) is the most well-known part, and a reference to it will show 
+    up later in the calendar. The final link is a Christmas song by Sia that was popular when it came out because there 
+    was a trend where couples would dance to it haha.`, 
     links: [{ text: "Frosty the Snowman Song", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=5ca6425942d54470" },
     { text: "The Snowman", url: "https://youtu.be/5A3THighARU?feature=shared" },
     { text: "Snowman by Sia", url: "https://open.spotify.com/track/3b95085PI7P3X3oC16Z3Wd?si=08c467f2c8b84f33" },] },
 
   { image: "assets/rudolph.jpg", 
     message: `Rudolph the red-nosed reindeer is the reindeer whose place is at the front of Santa’s sleigh. 
-    The sleigh has nine reindeer total, so the rest are  Dasher, Dancer, Prancer, Vixen, Comet and Cupid, and the 
-    final two "Dunder" and "Blixem" (meaning "thunder" and "lightning"). Because Rudolph’s nose is so bright, he 
+    The sleigh has nine reindeer total, and the rest are  Dasher, Dancer, Prancer, Vixen, Comet and Cupid, and the 
+    final two are "Dunder" and "Blixem" (meaning "thunder" and "lightning"). Because Rudolph’s nose is so bright, he 
     flies at the front to guide the sleigh in bad weather.`, 
     links: [{ text: "Rudolph the Red-Nosed Reindeer", url: "https://open.spotify.com/track/47otoIkhx3fkdivEXL5OB6?si=6590b4baaa974089" }] },
   
@@ -71,17 +71,17 @@ interface DayContent {
      The song gets off to a slow start, but once you get past the first part, it gets a little more lively. 
      It’s about an Irish couple who met and fell in love in New York. They grew old together and despite arguments (as in the part of the song where 
      they insult each other haha), they care about each other. A few notes on the lyrics: For the part where they say “Sinatra was swinging,”
-    they mean that music by Frank Sinatra (he appears later in the calendar) was playing The “NYPD choir” means the New York City Police Department's 
-    choir (actually the NYPD doesn’t have a choir; it only exists in the song haha)`, 
+    they mean that music by Frank Sinatra (he sings many of the songs in this calendar) was playing. The “NYPD choir” means the New York City Police Department's 
+    choir (actually the NYPD doesn’t have a choir; it only exists in the song haha).`, 
     links: [{ text: "Fairy Tale of New York Lyrics", url: "https://open.spotify.com/track/4soJpnUFA0SPs7XTi6QIVp?si=22a60cf4e3804473" },
       {text: "Fairy Tale of New York Performance", url: "https://youtu.be/eeOVR09ohSU?feature=shared"},] },
 
   { image: "assets/jackfrost2.jpg", 
     message: `Jack Frost is a legendary character (not from any specific story) who brings the cold and 
     frost of winter, giving people frostbite if they stay out too long. “The Christmas Song” mentions him
-     in the lyric “Jack Frost nipping at your nose.” “The Snow Queen” is a story written by Hans Christian Anderson
-      about a magical snow queen. She’s actually the antagonist in the seven-part story about good and evil. The links
-       above give a summary of the story, as well as some illustrations that later accompanied it.`,
+     in the lyric “Jack Frost nipping at your nose.” The Snow Queen is a story written by Hans Christian Anderson
+      about a magical snow queen. She’s actually the antagonist in the seven-part story about good and evil. The items
+       below give a summary of the story, as well as some illustrations that later accompanied it.`,
     links: [{ text: "Jack Frost Wikipedia", url: "https://en.wikipedia.org/wiki/Jack_Frost" },
       { text: "The Christmas Song", url: "https://open.spotify.com/track/4PS1e8f2LvuTFgUs1Cn3ON?si=84794bf573f5444c" },
       { text: "Summary of the Snow Queen", url: "https://interestingliterature.com/2020/03/snow-queen-fairy-tale-andersen-summary-analysis/" },
@@ -106,26 +106,23 @@ interface DayContent {
   { image: "assets/mistletoe.jpeg", 
     message: `Mistletoe is a plant with white berries that people hang up around Christmas, and the custom is that
      if two people are standing below it, they’re supposed to kiss. ~ Here’s a history explaining the reason why we kiss 
-     under mistletoe, as well as two famous Christmas songs that include lyrics about it. The singers of “Merry Christmas”
-      (Ed Sheeran & Elton John) are both really famous singers, and I’m sure you’ve heard songs from them before (even if 
-      you didn’t realize it was them singing). A quick note for the second song: In the “Christmas in Killarney” song, “beau”
-      means “boyfriend.”`, 
+     under mistletoe, as well as two famous Christmas songs that include lyrics about it. 
+     In the “Christmas in Killarney” song, “beau” means “boyfriend.”`, 
     links: [{ text: "Why do we kiss under mistletoe?", url: "https://youtu.be/QtJqBiF6EF0?feature=shared" },
       { text: "Merry Christmas by Ed Sheeran & Elton John", url: "https://youtu.be/Q_yuO8UNGmY?feature=shared" },
       { text: "Christmas in Killarney", url: "https://open.spotify.com/track/4XKj7iRK8QstOH4sG67YJf?si=e9bdfee53e2d4232" },] },
   
   { image: "assets/presentsleigh.jpeg", 
-    message: `The Twelve Days of Christmas is a Christmas season that celebrates the Nativity (the birth of Christ). 
+    message: `The Twelve Days of Christmas celebrates the Nativity (the birth of Christ). 
     It starts on Christmas Day, December 25th and lasts until January 5th. January 6th is Epiphany (or Three Kings Day), 
     the day when the Three Wise Men come to bring gifts to children in Mexico. The Twelve Days of Christmas is a classic 
     poem that many people sing around the holiday.`, 
     links: [{ text: "The Twelve Days of Christmas Poem", url: "https://www.poetryfoundation.org/poems/42913/the-twelve-days-of-christmas" },
       { text: "Twelve Days of Christmas by Bing Crosby", url: "https://open.spotify.com/track/0cAhE2iDJApegDnVRx6fCK?si=f700035963cd4680" },
-      { text: "The Office - 12 Days of Christmas Clip", url: "https://youtu.be/m29KmOSh0Mw?feature=shared" },
-      { text: "Best Christmas Ever - SNL", url: "https://youtu.be/4WvwX18oMR4?feature=shared" }] },
+      { text: "The Office - 12 Days of Christmas Clip", url: "https://youtu.be/m29KmOSh0Mw?feature=shared" }] },
   
   { image: "assets/peanutsputtinguptree.jpg", 
-    message: `“Peanuts” is an American comic strip that ran from 1950 to 2000, with various reruns after. You may
+    message: `“Peanuts” is an American comic strip that ran from 1950 to 2000, with various re-runs after. You may
      have heard of it before, but if not, you’ve at least certainly seen some of the characters from it (Snoopy!). 
      There are short films from the cartoon from various holidays, including Valentine’s Day, Halloween, Thanksgiving, 
      and of course, Christmas. :)`, 
@@ -151,23 +148,22 @@ interface DayContent {
       { text: "All My Bells are Ringing by Lenka", url: "https://open.spotify.com/track/4WrP4tyHau0Rq1RyHJZxqZ?si=2648fb526d2b4d8f" }] },
   
   { image: "assets/wisemen.jpg", 
-    message: `Las Posadas is a Catholic celebration lasting for nine days (From December 16th to December 24th), 
-    representing the Virgin Mary’s pregnancy. When I attended these as a kid, there was a half hour of praying the 
-    rosary first, which was followed by the singing (most people held candles for this part), then treat bags given 
-    by the host, and finally the piñata.`, 
+    message: `Las Posadas is a Catholic celebration lasting for nine days (from December 16th to December 24th). 
+    When I attended these as a kid, there was a half hour of praying the rosary first, which was followed by 
+    singing (most people held candles for that part), then treat bags given by the host, and finally the piñata.`, 
     links: [{ text: "El Pueblo de Las Posadas", url: "https://youtu.be/B3RqafZM9lM?feature=shared" },
       { text: "Las Posadas: A Mexican Christmas Tradition", url: "https://www.franciscanmedia.org/st-anthony-messenger/las-posadas-a-mexican-christmas-tradition/" },
       { text: "Las Posadas Mexicanos", url: "https://youtu.be/hx1_CKrWrjo?feature=shared" },] },
   
   { image: "assets/fruitcake.jpeg", 
-    message: `The Grinch is a cartoon character from authors Dr. Suess’s “The Grinch” book, published in 1957. 
+    message: `You don't have to watch the entire video, but the Grinch is a cartoon character from Dr. Suess’s “The Grinch” book, published in 1957. 
     In his story, the Grinch is a mean hateful creature who despises Christmas (he's the character from the Cracker 
     Barrel who you said looked like the Duolingo owl haha). The village below where he lives 
     is called Whoville, and it has many creatures who love and always celebrate Christmas. Since he hates the Whos, 
     on Christmas Eve he decides to go from house to house to steal all of their gifts in decorations, thinking “haha! 
     Imagine how rotten they will feel in the morning!” To his surprise, when they wake up, they still see Christmas 
-    carols and enjoy the holiday, and he realizes that the holiday sentiment is more than just the food and presents, 
-    and his heart grows three sizes. Because of this story, people sometimes still refer to a hateful person as a “grinch.”`,
+    carols and enjoy the holiday, and he realizes that the holiday sentiment isn't just about the food and presents. 
+    After his epiphany, his heart grows three sizes bigger. Because of this story, people sometimes still refer to a hateful person as a “grinch.”`,
     links: [{ text: "How the Grinch Stole Christmas", url: "https://dai.ly/x3ie7xt" }, 
       {text: "Grinch Wikipedia", url: "https://en.wikipedia.org/wiki/Grinch"}] },
 
@@ -184,40 +180,66 @@ interface DayContent {
 
   { image: "assets/homeforchristmas.jpg", 
     message: `For today, here’s one song about celebrating the holidays at home, and 
-    the second link is a scene from the movie “A Christmas Story” in which the father wins an award for completing 
+    the second link is a scene from the movie A Christmas Story in which the father wins an award for completing 
     crossword puzzles in the newspaper.`, 
     links: [{ text: "(There's No Place Like) Home for the Holidays by Perry Como", url: "https://open.spotify.com/track/0ZamzoOYLzRxE3WuOb5J3r?si=d4b1d83718654d99" },
       { text: "A Christmas Story - The Leg Lamp is Delivered", url: "https://youtu.be/F-Vz67p5kLQ?feature=shared" }] },
 
   { image: "assets/greenhouse.jpg", 
     message: `Gingerbread houses and gingerbread men are another fun part of Christmas festivities, and they originated 
-    in part from the witch’s candy house in the German fairytale of “Hansel and Gretel.” The second link here is one of 
-    the most popular Christmas songs of all time haha. You have another gift waiting for you in my locker. ~`, 
+    in part from the witch’s candy house in the German fairytale of Hansel and Gretel. The second and third links here are two of 
+    the most popular Christmas songs of all time haha. You have another gift waiting for you in the locker haha. ~`, 
     links: [{ text: "A Brief History of the Gingerbread House", url: "https://www.theguardian.com/travel/2018/dec/22/a-brief-history-of-the-gingerbread-house" },
       { text: "All I Want For Christmas is You by Mariah Carey", url: "https://youtu.be/yXQViqx6GMY?feature=shared" },
       { text: "Last Christmas by Wham!", url: "https://open.spotify.com/track/2FRnf9qhLbvw8fu4IBXx78?si=c46635a1e25d4798" },
     ] },
 
   { image: "assets/wintericeskating.jpg", 
-    message: `Here are three songs about the snowy winter. ~ The first is a cover that I’d never heard before, 
-    but I think it sounds nice. The second is from a famous Christmas movie featuring Bing Crosby, and the third 
+    message: `Here are three songs about snowy winter. ~ The first is a cover that I hadn't heard before, 
+    but I think it sounds nice. The second is from the movie White Christmas, and the third 
     is a classic Christmas song about welcoming the falling snow. `, 
     links: [{ text: "Winter Wonderland", url: "https://open.spotify.com/track/7CeP2uqPli3AW6Fn6OF70L?si=781b2573cb0943ce" },
       { text: "White Christmas by Bing Crosby", url: "https://open.spotify.com/track/3XsaSIPWvM61RIFfeb0BBR?si=9b01098775d34c17" },
       { text: "Let it Snow! by Robert Goulet", url: "https://open.spotify.com/track/6dx8Z821RaAqjuoJafsM8X?si=b14690340e0c4afb" }
     ] },
 
-  { image: "assets/santasleigh.jpeg", message: "here is the message", links: [{ text: "Luke 2", url: "https://tinyurl.com/5ctx6u3v" }] },
+  { image: "assets/santasleigh.jpeg", 
+    message: `Christmas can also take a role in theater! These are some Christmas songs from the classic movies, 
+    Neptune’s Daughter, Meet Me in St. Louis, and White Christmas. The blue-eyed man from White Christmas is 
+    Bing Crosby, the same one who sings many of the other songs in this calendar haha. The final link is a scene 
+    from a newer movie, The Holdovers, which won many awards when it came out. The older man in the scene works 
+    at a boarding school and is responsible for taking care of a stubborn student (who had to stay there alone over 
+    break haha.)`, 
+    links: [{ text: "Baby, It's Cold Outside (from Neptune's Daughter)", url: "https://youtu.be/7MFJ7ie_yGU?feature=shared" },
+      { text: "Merry Little Christmas (from Meet Me in St. Louis)", url: "https://youtu.be/CreWsnhQwzY?feature=shared" },
+      { text: "Snow (from White Christmas)", url: "https://youtu.be/G47k6qSKgPw?feature=shared" },
+      { text: "Sisters (from White Christmas)", url: "https://youtu.be/PG7x8HWbDzU?feature=shared" },
+      { text: "'Sisters' (from White Christmas)", url: "https://youtu.be/DDyybi7t634?feature=shared" },
+      { text: "The Holdovers Scene", url: "https://youtu.be/ME7ZRs5kQxM?feature=shared" },
+    ] },
 
   { image: "assets/bluebells.jpg", 
-    message: "here is the message", 
-    links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
+    message: `Bells have long been associated with Christmas because of their religious significance (church bells), 
+    symbolism of celebration, and their association with Santa’s sleigh (the legend that you can hear his sleigh
+    bells ringing as he gets nearer). So here are four songs that reference Christmas bells!`, 
+    links: [{ text: "Jingle Bells by Frank Sinatra", url: "https://open.spotify.com/track/4KV9bM7a1KDc7b7OakFZic?si=66701ceb1faa43bc" },
+      { text: "Silver Bells by Bing Crosby", url: "https://open.spotify.com/track/6ukZegLjDkrDQWFYsQ2ODt?si=bdb17ad5e63a426a" },
+      { text: "Jingle Bell Rock by Bobby Helms", url: "https://open.spotify.com/track/7vQbuQcyTflfCIOu3Uzzya?si=835a85b641424282" },
+      { text: "Sleigh Ride by the Ronettes", url: "https://open.spotify.com/track/5ASM6Qjiav2xPe7gRkQMsQ?si=d74154d4ffd34475" },
+    ] },
 
-  { image: "assets/store.jpeg", message: "here is the message", links: [{ text: "Merry Christmas to all", url: "https://open.spotify.com/track/35MqcEIVZ7svbBpRt4N30g?si=e85bf43813564a7d" }] },
+  { image: "assets/store.jpeg", 
+    message: `I’m not going to write too much here, since I plan to either tell you some of these things in person or write them in a card today, 
+    but I do want to say that this past year with you may have been the best one of my life. I love you, and Merry Christmas. ~`, 
+    links: [{ text: "", url: "" }] },
 
   { image: "assets/maryandjoseph.jpg", 
-    message: "here is the message", 
-    links: [{ text: "What Child is This? by Bing Crosby", url: "https://open.spotify.com/track/6OJdXv1Q5OdLgYolFEunJ4?si=9343d037ddc7411a" }] },
+    message: `It’s finally Christmas Day now! Since this is the day most people would celebrate Christmas as a religious holiday, 
+    going to church and listening to choir music, the three items for today are related to the original Christmas story. `, 
+    links: [{ text: "Luke 2 Reading", url: "https://www.biblegateway.com/passage/?search=Luke%202&version=NIV&interface=print" },
+      {text: "The First Noel by Frank Sinatra", url: "https://open.spotify.com/track/4WW2Bdai1qzTnNkWNJCGNg?si=fe23471d95444014"},
+      {text: "What Child is This?", url: "https://open.spotify.com/track/6PtzDFf5LbDUqZdQKvx13U?si=c15de858715e43fe"}
+    ] },
 ];
  function createCalendar() {
   const calendar = document.getElementById("calendar");
